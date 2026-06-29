877729d2ff9a2ba5e62c64e7179f9ea7ed7e2d58
Ming <minjoo255@gmail.com>
2026-06-29 15:19:30 +0900

    운영시간 수정: 토요일 22:00, 일요일 21:40으로 교환

--- src/data/store.js
+++ src/data/store.js
@@ -6,8 +6,8 @@ export const STORE_INFO = {
   lotAddress: '충남 천안시 동남구 신부동 461-10',
   hours: [
     { label: '평일', time: '12:00 ~ 21:40' },
-    { label: '토요일', time: '11:00 ~ 21:40' },
-    { label: '일요일', time: '11:00 ~ 22:00' },
+    { label: '토요일', time: '11:00 ~ 22:00' },
+    { label: '일요일', time: '11:00 ~ 21:40' },
   ],
   naverPlaceUrl:
     'https://map.naver.com/p/entry/place/1266955305?placePath=/home?entry=plt&from=map&fromPanelNum=1&additionalHeight=76&timestamp=202606091547&locale=ko&svcName=map_pcv5&searchType=place&lng=127.1555342&lat=36.8183174&c=16.00,0,0,0,dh',
